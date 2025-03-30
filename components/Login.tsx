27618fbee3360bfe921434620d8996054a395d22
RaaphaelGomesS <rafagomes1706@gmail.com>
2025-03-30 18:35:42 -0300

    add base project

---
+++ components/Login.tsx
@@ -0,0 +1,22 @@
+import  React, {useState} from 'react';
+import {Text, View} from 'react-native';
+import { StyleSheet } from 'react-native';
+
+const Login = () => {
+
+    const [username, setUsername] = useState(''); 
+    const [password, setPassword] = useState(''); 
+
+  return (
+    <View>
+
+    </View>
+  );
+};
+
+export default Login;
+
+const style = StyleSheet.create({
+
+
+});
